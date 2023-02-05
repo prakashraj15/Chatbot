bf9a460b5c740c2a0a84e9e8e421c87986ac7f42
P Mohana Prasad <p.mohanaprasad@outlook.com>
2023-02-05 22:02:34 +0530

    app.py

--- chatbot/chatbot/chat.py
+++ chatbot/chatbot/chat.py
@@ -8,10 +8,10 @@ from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('intents.json', 'r') as json_data:
+with open('chatbot\chatbot\intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
-FILE = "data.pth"
+FILE = "chatbot\chatbot\data.pth"
 data = torch.load(FILE)
 
 input_size = data["input_size"]
@@ -26,6 +26,7 @@ model.load_state_dict(model_state)
 model.eval()
 
 bot_name = "Sam"
+
 
 def get_response(msg):
     sentence = tokenize(msg)
@@ -44,7 +45,7 @@ def get_response(msg):
         for intent in intents['intents']:
             if tag == intent["tag"]:
                 return random.choice(intent['responses'])
-    
+
     return "I do not understand..."
 
 
@@ -58,4 +59,3 @@ if __name__ == "__main__":
 
         resp = get_response(sentence)
         print(resp)
-
